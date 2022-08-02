7a91d0a596298ccba2e738683046d23422113bec
jf <j@b.f>
2022-08-02 16:02:36 +0100

    bit better stars

--- src/concepts/EscapeVelocity.hx
+++ src/concepts/EscapeVelocity.hx
@@ -9,68 +9,102 @@ import peote.view.Color;
 class EscapeVelocity extends FullScene {
 	var starRenderer:ShapeRenderer;
 	var stars:Array<Star>;
+	var speed:Float;
 
 	override function create() {
 		super.create();
-		starRenderer = stage.createShapeRenderLayer("stars");
-
+		starRenderer = stage.createShapeRenderLayer("stars", false, true, this.width, this.height);
+		var layer = stage.getLayer("stars");
+		@:privateAccess
+		layer.frameBuffer.display.xOffset = -64;
 		// generate star range
-		var numStars = 100;
-
+		var numStars = 85;
+		speed = 3;
 		stars = [
 			for (i in 0...numStars) {
-				var x = randomInt(64);
+				var x = randomInt(this.width);
 				var y = randomInt(64);
 				var color = Color.WHITE;
-				var speedOption = randomInt(3);
-				var speed = switch speedOption {
-					case 1: -0.05;
-					case 2: -0.7;
-					case 3: -0.5;
+				var distanceChance = randomInt(3);
+				var distanceFactor = switch distanceChance {
+					// case 1: -0.05;
+					// case 2: -0.7;
+					// case 3: -0.5;
+					// case _: -0.01;
+					// case 1: -0.05;
+					case 2: -0.9;
+					case 3: -0.2;
 					case _: -0.01;
 				};
-				var alpha = 200 * (1 - speed) + 100;
+				var minAlpha = 90;
+				var maxAlpha = (222 - minAlpha);
+				var alpha = (maxAlpha * (-distanceFactor)) + minAlpha;
 				color.alpha = Std.int(alpha);
-
-				{
-					x: x,
-					speed: speed,
-					shape: starRenderer.makeShape(x, y, 1, 1, RECT, color)
-				}
+				new Star(x, y, distanceFactor, starRenderer.makeShape(x, y, 1, 1, RECT, color));
 			}
+
 		];
 
-		var speedUpPause = 2;
+		var speedUpPause = 0.4;
 		behaviours = [
 			new CountDown(speedUpPause, () -> {
-				trace('speed up');
+				speed *= 1.29;
+				if (speed > 40) {
+					speed = 40;
+				}
 			}, true),
-			new CountDown(0, () -> {
+			new CountDown(0.2, () -> {
 				for (star in stars) {
-					star.update();
-					if (star.x < 0) {
-						star.setX(64);
-					}
+					star.update(speed);
 				}
 			}, true),
 		];
 	}
 }
 
-@:structInit
 class Star {
 	public var shape:Shape;
 	public var x:Float;
 
-	var speed:Float;
+	var y:Int;
+	var xOffset:Int;
+	var xTruncLast:Int = 0;
+	var distanceFactor:Float;
+
+	public function new(xOffset:Int, y:Int, distanceFactor:Float, shape:Shape) {
+		this.xOffset = xOffset;
+		this.x = xOffset;
+		this.y = y;
+		this.distanceFactor = distanceFactor;
+		this.shape = shape;
+	}
 
 	inline function shiftX(increment:Float) {
 		x += increment;
-		shape.x = Math.floor(x);
+		var xTrunc = Math.floor(x);
+		var distanceTravelled = xTrunc - xTruncLast;
+		var trailLength = 1;
+		var w = Std.int(distanceTravelled * distanceFactor);
+		if (w > 1) {
+			trailLength = w;
+		}
+		if (w > 40) {
+			trailLength = 40;
+		}
+		shape.w = trailLength;
+		shape.x = xTrunc - (trailLength);
+		xTruncLast = xTrunc;
+		if (x < 64) {
+			// xTruncLast = 0;
+			// w = 1;
+			// x = 128;
+			x = this.x = xOffset;
+			trace('reset x $x');
+		}
 	}
 
-	public function update() {
-		shiftX(speed);
+	public function update(speed:Float) {
+		shiftX(speed * distanceFactor);
 	}
 
 	public function setX(nextX:Int) {
