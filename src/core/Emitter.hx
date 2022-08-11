fa775a33a6f428cc0ddd387fd9f0b1e8765577b3
jf <j@b.f>
2022-08-12 00:03:03 +0100

    more efficient emitter

--- src/core/Emitter.hx
+++ src/core/Emitter.hx
@@ -26,64 +26,78 @@ class Emitter {
 		world.step(elapsedSeconds);
 	}
 
-	var sprites:Array<Sprite> = [];
+	var particles:Array<Particle> = [];
 
 	public function emit(x:Float, y:Float, x_vel:Float, y_vel:Float, tileId:Int) {
-		var sprite = tiles.makeSprite(Std.int(x), Std.int(y), 16, tileId);
-		var body = new Body({
-			shape: {
-				// type: type,
-				solid: false,
-				// radius: radius,
-				width: 1,
-				height: 1,
-				// sides: sides,
-				// vertices: vertices,
-				// rotation: rotation,
-				// scale_x: scale_x,
-				// scale_y: scale_y,
-				// offset_x: offset_x,
-				// offset_y: offset_y
-			},
-			// shapes: shapes,
-			// shape_instance: shape_instance,
-			// shape_instances: shape_instances,
-			// material: material,
-			kinematic: false,
-			mass: 1,
-			x: x,
-			y: y,
-			// rotation: rotation,
-			// scale_x: scale_x,
-			// scale_y: scale_y,
-			// elasticity: elasticity,
-			velocity_x: x_vel,
-			velocity_y: y_vel,
-			rotational_velocity: randomFloat(100,300),
-			// max_velocity_x: max_velocity_x,
-			// max_velocity_y: max_velocity_y,
-			// max_velocity_length: max_velocity_length,
-			// max_rotational_velocity: max_rotational_velocity,
-			// drag_x: drag_x,
-			// drag_y: drag_y,
-			// drag_length: drag_length,
-			// rotational_drag: rotational_drag,
-			// gravity_scale: gravity_scale
-		});
-		sprites.push(sprite);
-		body.data.sprite = sprite;
-		body.on_move = (x, y) -> {
-			//  sprite.setPosition(,y * 10);?
-			sprite.x = Std.int(x); // * 10);
-			sprite.y = Std.int(y); // * 10);
-			//  trace('move $x $y ${sprite.x} ${sprite.y}');
-		};
+		var maxParticles:Int = 20;
+		if (particles.length < maxParticles) {
+			var sprite = tiles.makeSprite(Std.int(x), Std.int(y), 16, tileId);
+			var p:Particle = {
+				sprite: sprite,
+				body: new Body({
+					shape: {
+						solid: false,
+						width: 1,
+						height: 1,
+					},
+					kinematic: false,
+					mass: 1,
+					x: x,
+					y: y,
+					velocity_x: x_vel,
+					velocity_y: y_vel,
+					rotational_velocity: randomFloat(100, 300),
+				})
+			}
+			p.body.data.sprite = sprite;
+			p.body.on_move = (x, y) -> {
+				//  sprite.setPosition(,y * 10);?
+				sprite.x = Std.int(x); // * 10);
+				sprite.y = Std.int(y); // * 10);
+				if (x < -32 || x > 72) {
+					p.isRecycle = true;
+				}
+				if (y < -32 || y > 72) {
+					p.isRecycle = true;
+				}
 
-		body.on_rotate = r -> sprite.rotation = r;
-		world.add(body);
+				//  trace('move $x $y ${sprite.x} ${sprite.y}');
+			};
+			p.body.on_rotate = r -> sprite.rotation = r;
+			world.add(p.body);
+			particles.push(p);
+		} else {
+			for(p in particles){
+				if(p.isRecycle){
+					p.isRecycle = false;
+					p.body.x = x;
+					p.body.y = y;
+					p.body.velocity.x = x_vel;
+					p.body.velocity.y = y_vel;
+					p.body.rotational_velocity = randomFloat(100, 300);
+					break;
+				}
+			}
+			// var readyParticles = particles.filter(particle -> particle.isRecycle);
+			// if (readyParticles.length >= 0) {
+			// 	var p = readyParticles[0];
+			// 	p.body.x = x;
+			// 	p.body.y = y;
+			// 	p.body.velocity.x = x_vel;
+			// 	p.body.velocity.y = y_vel;
+			// 	p.body.rotational_velocity = randomFloat(100, 300);
+			// }
+		}
 	}
 
 	var world:World;
 
 	var tiles:SpriteRenderer;
+}
+
+@:structInit
+class Particle {
+	public var sprite:Sprite;
+	public var body:Body;
+	public var isRecycle:Bool = false;
 }
